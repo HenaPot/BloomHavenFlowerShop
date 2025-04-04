00ff4d6c0b7e54d1a1511ee9581e7e570e63207f
HenaPot <hena.potogija@stu.ibu.edu.ba>
2025-04-04 20:53:55 +0200

    item in order backend

--- backend/index.php
+++ backend/index.php
@@ -7,5 +7,6 @@ require 'rest/routes/shopping_cart_routes.php';
 require 'rest/routes/auth_routes.php';
 require 'rest/routes/wishlist_routes.php';
 require 'rest/routes/order_routes.php';
+require 'rest/routes/item_in_order_routes.php';
 
 Flight::start();
