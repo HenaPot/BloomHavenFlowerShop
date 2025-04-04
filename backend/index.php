4709ab404f89aabcefef67e25fbbc756c5755d8f
HenaPot <hena.potogija@stu.ibu.edu.ba>
2025-04-04 19:40:23 +0200

    order CRUD backend

--- backend/index.php
+++ backend/index.php
@@ -6,5 +6,6 @@ require 'rest/routes/user_routes.php';
 require 'rest/routes/shopping_cart_routes.php';
 require 'rest/routes/auth_routes.php';
 require 'rest/routes/wishlist_routes.php';
+require 'rest/routes/order_routes.php';
 
 Flight::start();
