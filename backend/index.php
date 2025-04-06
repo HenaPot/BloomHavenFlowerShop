f32fc140a5195091c2db433157912d1655120ff5
HenaPot <hena.potogija@stu.ibu.edu.ba>
2025-04-06 15:25:02 +0200

    product backend CRUD completed

--- backend/index.php
+++ backend/index.php
@@ -8,5 +8,6 @@ require 'rest/routes/auth_routes.php';
 require 'rest/routes/wishlist_routes.php';
 require 'rest/routes/order_routes.php';
 require 'rest/routes/item_in_order_routes.php';
+require 'rest/routes/product_routes.php';
 
 Flight::start();
