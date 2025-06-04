477461a0c7b8dc5076b5b50534e5be873f3e0c8e
HenaPot <hena.potogija@stu.ibu.edu.ba>
2025-06-04 19:43:23 +0200

    Added dynamic picture loading on all products screen

--- frontend/services/product-service.js
+++ frontend/services/product-service.js
@@ -314,9 +314,15 @@ openDeleteConfirmationDialog: function (productStr) {
         }
 
         products.forEach(product => {
+        // Get the first image URL or fallback
+        const imageUrl = (product.images && product.images.length > 0)
+          ? 'backend/' + product.images[0].image // assuming image paths start with `/uploads/...`
+          : 'frontend/assets/images/kvalitetno_cvijece.webp'; // your default placeholder image
+
           container.innerHTML += `
             <div class="col-lg-4 col-md-6 mb-4">
               <div class="card h-100">
+                <img src="${imageUrl}" class="card-img-top" style="height: 200px; object-fit: cover;" alt="Product Image">
                 <div class="card-body">
                   <h5 class="card-title mb-3">${product.name}</h5>
                   <p class="mb-1"><strong>Category:</strong> ${product.category_name}</p>
@@ -327,7 +333,7 @@ openDeleteConfirmationDialog: function (productStr) {
               </div>
             </div>
           `;
-        });
+      });
       },
       function () {
         document.getElementById("products-list").innerHTML =
