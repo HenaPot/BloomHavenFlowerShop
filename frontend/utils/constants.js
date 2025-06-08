207f87bed3dad471a603bd3edde124a79884f273
HenaPot <hena.potogija@stu.ibu.edu.ba>
2025-06-08 16:54:26 +0200

    Removed double slash from URLs

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,9 +1,9 @@
 var Constants = {
     get_api_base_url: function () {
         if (location.hostname === "localhost") {
-            return "http://localhost/WebProjekat/backend/";
+            return "http://localhost/WebProjekat/backend";
         } else {
-            return "https://bloomhaven-flowershop-app-govf2.ondigitalocean.app/";
+            return "https://bloomhaven-flowershop-app-govf2.ondigitalocean.app";
         }
     },
     USER_ROLE: "user",
